216858bbd6d5c7fe707ecfad2903a29b3ca33af9
Thomas Park <thomas@thomaspark.me>
2012-04-10 15:30:24 -0400

    update create account page

--- create.php
+++ create.php
@@ -36,7 +36,7 @@ if($_REQUEST['action'] == 'create'){
 										<input name="action" id="action" value="create" type="hidden">
 										<div><input name="submit" id="submit" value="Create Account" type="submit"></div>
 									</form>
-									
+									<p style="text-align: center;"><a href="./"  style="color: #888;">Already Have Account</a></p>
 									<div class="warning">Please try again.</div>
 
 						</body>
@@ -60,20 +60,37 @@ if($_REQUEST['action'] == 'create'){
 				<link rel="stylesheet" href="css/style.css" type="text/css" />
 		</head>
 		<body>
-			<div id="control"><div class="control"><div class="buttons"><div id="auth"><span id="logo">openHTML</span></div></div></div></div><div id="bin" class="stretch">
+			<div id="control">
+				<div class="control">
+					<div class="buttons">
+						<div id="auth">
+							<span id="logo">openHTML</span>
+						</div>
+					</div>
+					</div>
+				</div>
+			<div id="bin" class="stretch">
 
 				<form name="createformname" method="post" id="createformname" class="loginformid" enctype="application/x-www-form-urlencoded" action="/openhtml/create.php">
 						<h1>Create Account</h1>
-						<div><label for="username">Username (required)</label>
-						<input name="username" id="username" class="required" type="text" maxlength="20" autofocus></div>
-						<div><label for="password">Password (required)</label>
-						<input name="password" id="password" class="required" type="password"></div>
-						<div><label for="email">Email (optional)</label>
-						<input name="email" id="email" type="text"></div>
+						<div>
+							<label for="username">Username (required)</label>
+							<input name="username" id="username" class="required" type="text" maxlength="20" autofocus>
+						</div>
+						<div>
+							<label for="password">Password (required)</label>
+							<input name="password" id="password" class="required" type="password">
+						</div>
+						<div>
+							<label for="email">Email (optional)</label>
+							<input name="email" id="email" type="text">
+						</div>
 						<input name="action" id="action" value="create" type="hidden">
 						<div><input name="submit" id="submit" value="Create Account" type="submit"></div>
-					</form>
+			</form>
+					<p style="text-align: center;"><a href="./" style="color: #888;">Already Have Account</a></p>
 				
+				</div>
 		</body>
 </html>
 
