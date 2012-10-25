9a9410bbdb2a6423e8af069965bc9a3d5d938dd1
Ankur <as3445@drexel.edu>
2012-10-25 02:20:46 -0400

    huge mess

--- index.php
+++ index.php
@@ -58,21 +58,47 @@ $ownership = checkOwner($code_id, $revision, $_SESSION['name']);
 <!--[if gte IE 8 ]><body class="source ie"><![endif]-->
 <!--[if !IE]><!--><body class="source"><!--<![endif]-->
 
+<!-- sends validation data -->
 <form name=validate action="/openhtml/validate.php" method=post>
   <input type=hidden name="html_code" value= '<?php echo $html; ?>
-  <input type=hidden name="test" value="Test Value">
+  <input type=hidden name="test" value="Test Value">'>
 </form>
 
+<!-- top panel -->
 <div id="control">
   <div class="control">
     <div class="buttons">
-      <a id="account" class="tab button group light left" href="<?php echo ROOT?>list">Page List<?php //echo $is_owner?></a>
-      <a id="account" class="tab button group light" href="<?php echo ROOT?>">New Page</a>
-      <a id="account" class="tab button group light right gap" href="#" onclick="document.forms['validate'].submit(); return false;">Validate</a>
+      <a id="account" class="tab button gap" href="<?php echo ROOT?>">New Page</a>
+
+      <a id="jsbinurl" target="<?php echo $code_id?>" class="tab button group light left" href="http://<?php echo $_SERVER['HTTP_HOST'] . ROOT . $code_id?>">View</a>
+
+        <?php if ($code_id) : ?>
+          <a id="save" title="Save a new revision" class="button light save group" href="<?php echo $code_id_path?>save">Save</a>
+        <?php else : ?>
+          <a id="save" class="tab button light save group" href="<?php echo ROOT?>save">Save</a>
+        <?php endif ?>
+
+        <?php if ($ownership) :?>
+                <div class="button group gap right tall">
+                <a id="options" class="title">Options</a>
+                <a id="save" title="Save a new revision" class="button light save group" href="<?php echo $code_id_path?>save">Save</a>
+                <a id="clone" title="Create a new copy" class="button clone group light" href="<?php echo ROOT?>clone">Copy</a>
+
+            <?php else : ?>
+                <div class="button group gap right short">
+                <a id="options" class="title">Options</a>
+                <a id="clone" title="Create a new copy" class="button clone group light" href="<?php echo ROOT?>clone">Copy</a>
+            <?php endif ?>
+           <a id="download" title="Save to drive" class="button download group light" href="<?php echo ROOT?>download">Download</a>
+           <!--<a id="account" target="<?php echo $code_id?>"class="button group light" href="#" onclick="document.forms['validate'].submit(); return false;">Validate</a> -->
+
+      </div> 
+      
+      
       <!--<a class="tab button source group left" accesskey="1" href="#source">Code</a>
       <a class="tab button preview group right gap" accesskey="2" href="#preview">Preview</a>-->
       <a title="Revert" class="button light group left" id="revert" href="#"><img class="enabled" src="<?php echo ROOT?>images/revert.png" /><img class="disabled" src="<?php echo ROOT?>images/revert-disabled.png" /></a>
-    <?php if ($code_id) : ?>
+    <!--<?php if ($code_id) : ?>
       <a id="jsbinurl" target="<?php echo $code_id?>" class="button group light left" href="http://<?php echo $_SERVER['HTTP_HOST'] . ROOT . $code_id?>"><?php echo $_SERVER['HTTP_HOST'] . ROOT . $code_id ?></a>
 
 
@@ -95,25 +121,26 @@ $ownership = checkOwner($code_id, $revision, $_SESSION['name']);
           <a id="save" title="Save new bin" class="button save group" href="<?php echo ROOT?>save">Save</a>
       <?php endif ?>
           <a id="download" title="Save to drive" class="button download group light" href="<?php echo ROOT?>download">Download</a>
-          <!-- <a id="startingpoint" title="Set as starting code" class="button group" href="<?php echo ROOT?>save">As template</a> -->
-      </div>
+          <!-- <a id="startingpoint" title="Set as starting code" class="button group" href="<?php echo ROOT?>save">As template</a> 
+      </div>  -->
 
-      <span id="panelsvisible" class="gap">View:
+      <span id="panelsvisible" class="gap">Show:
         <input type="checkbox" data-panel="javascript" data-uri="css" id="showjavascript"><label for="showjavascript">CSS</label>
         <input type="checkbox" data-panel="html" data-uri="html" id="showhtml"><label for="showhtml">HTML</label>
-        <input type="checkbox" data-panel="live" data-uri="live" id="showlive"><label for="showlive">Live</label>
+        <input type="checkbox" data-panel="live" data-uri="live" id="showlive"><label for="showlive">Preview</label>
       </span>
 
       <div id="userinfo">
-        <a id="account" class="button group light left" href="<?php echo ROOT?>list"><?php echo $_SESSION['name']; ?></a>
-        <a id="logout" class="button group light right" href="<?php echo ROOT?>logout">Logout</a>
+        <a id="account" class="button group light left" href="<?php echo ROOT?>list">Page List<?php //echo $is_owner?></a> 
+        <!--<a id="account" class="button group light" href="<?php echo ROOT?>list"><?php echo $_SESSION['name']; ?></a> -->
+        <a id="logout" class="button group light right" href="<?php echo ROOT?>logout">Logout: <?php echo $_SESSION['name']; ?></a>
       <span id="logo">openHTML</span>
     </div>
     </div>
   </div>
-
-
 </div>
+
+<!-- text area -->
 <div id="bin" class="stretch" style="opacity: 0; filter:alpha(opacity=0);">
   <div id="source" class="binview stretch">
     <div class="code stretch javascript">
@@ -135,6 +162,7 @@ $ownership = checkOwner($code_id, $revision, $_SESSION['name']);
     <input type="hidden" name="method" />
   </form>
 </div>
+
 
 <script>
 
