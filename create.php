7eaa107ba0c1b2ee51ddc461b696282c91166748
Ankur <as3445@drexel.edu>
2013-01-08 12:54:21 -0500

    Ability to consent into research
    consent button, formatting, etc

--- create.php
+++ create.php
@@ -5,18 +5,7 @@ include('logger.php');
 $log = new logmein();
 $log->encrypt = true; //set encryption
 
-
-if(isset($_REQUEST['action']) == 'create'){
-    if($log->create("ownership", $_POST['username'], $_POST['password'], $_POST['email'], $_POST['section']) == true){
-        //do something on successful creation
-        
-        // logger('login');
-			$log->logout();
-			header('Location: ./');
-    }else{
-        //do something on FAILED login
-		
-			echo '	<!DOCTYPE html>
+$loginform = '	<!DOCTYPE html>
 					<html>
 						<head>
 								<title>openHTML - Create Account</title>
@@ -35,69 +24,41 @@ if(isset($_REQUEST['action']) == 'create'){
 										<input name="email" id="email" type="text"></div>
 										<div><label for="section">Group (optional)</label>
 										<input name="section" id="section" type="text"></div>
+										<div class="message">
+										<input name="consent" id="section" type="checkbox" checked="checked" style="display: block; float:left; margin-right: 15px;" />
+										<label for="section" style="margin-left:0px;">
+										Allow anonymous data to be used for research.</label>
+										</div>
 										<input name="action" id="action" value="create" type="hidden">
-										<div><input name="submit" id="submit" value="Create Account" type="submit"></div>
+										<div style="text-align: center;"><input name="submit" id="submit" class="button medium" style="height:auto; float:none;" value="Create Account" type="submit"></div>
 									</form>
 									<p style="text-align: center;"><a href="./"  style="color: #888;">Already Have Account</a></p>
-									<div class="warning">Please try again.</div>
-
 						</body>
 				</html>
 			';
+
+if(isset($_REQUEST['action']) == 'create'){
+    if($log->create("ownership", $_POST['username'], $_POST['password'], $_POST['email'], $_POST['section'], $_POST['consent']) == true){
+        //do something on successful creation
+        
+        // logger('login');
+			$log->logout();
+			header('Location: ./');
+    }else{
+        //do something on FAILED login
+		//echo loginform and fail msg
+
+			echo $loginform .
+								'
+								<div class="warning" style="text-align: center;">Please try again.</div>';
 			
 			die();
     }
 }
 
 
-
+echo $loginform;
 
 ?>
-
-
-<!DOCTYPE html>
-	<html>
-		<head>
-				<title>openHTML - Create Account</title>
-				<link rel="stylesheet" href="css/style.css" type="text/css" />
-		</head>
-		<body>
-			<div id="control">
-				<div class="control">
-					<div class="buttons">
-						<div id="auth">
-							<span id="logo">openHTML</span>
-						</div>
-					</div>
-					</div>
-				</div>
-			<div id="bin" class="stretch">
-
-				<form name="createformname" method="post" id="createformname" class="loginformid" enctype="application/x-www-form-urlencoded" action="/openhtml/create.php">
-						<h1>Create Account</h1>
-						<div>
-							<label for="username">Username (required)</label>
-							<input name="username" id="username" class="required" type="text" maxlength="20" autofocus>
-						</div>
-						<div>
-							<label for="password">Password (required)</label>
-							<input name="password" id="password" class="required" type="password">
-						</div>
-						<div>
-							<label for="email">Email (optional)</label>
-							<input name="email" id="email" type="text">
-						</div>
-						<div>
-							<label for="section">Group (optional)</label>
-							<input name="section" id="section" type="text">
-						</div>
-						<input name="action" id="action" value="create" type="hidden">
-						<div><input name="submit" id="submit" value="Create Account" type="submit"></div>
-			</form>
-					<p style="text-align: center;"><a href="./" style="color: #888;">Already Have Account</a></p>
-				
-				</div>
-		</body>
-</html>
 
 
