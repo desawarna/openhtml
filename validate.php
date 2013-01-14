2afd301f29374224564c89b500a27fc76328f49d
Thomas Park <thomas@thomaspark.me>
2013-01-14 15:27:11 -0500

    disabled soap in validator

--- validate.php
+++ validate.php
@@ -9,7 +9,8 @@ function validate($code, $type){
     $handle,
     array(
       CURLOPT_URL => $url,
-      CURLOPT_POSTFIELDS => "fragment=".$code."&output=soap12",
+      CURLOPT_POSTFIELDS => "fragment=".$code,
+      // CURLOPT_POSTFIELDS => "fragment=".$code."&output=soap12",
       CURLOPT_RETURNTRANSFER => true
     )
   );
