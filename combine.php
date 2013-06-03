b9bd8248319cfffe9faf9f8fc9e1ac2275323f5a
Ankur <as3445@drexel.edu>
2013-06-03 10:54:29 -0400

    combined sessions using arrays instead of string manipulations

--- combine.php
+++ combine.php
@@ -5,7 +5,7 @@
 
 //Variables
 	$pages = array(); // Contains a Row each entry which includes consolidated data for each page from the old data set
-	$combined = "[";
+	$combined = array();
 	$url = "";
 	$purl = "x";
 	$i = 0;
@@ -24,48 +24,45 @@
 	// $r = mysql_fetch_assoc($s);
 
 	while($r = mysql_fetch_assoc($s, MYSQL_ASSOC)){
-		// echo $r[url]."-".$r[customname]."-".$r[time]."-".$r[html]."-".$r[css]."-".$r[special]."***<br />";
-		// echo '{
-		// 		"clock":'		. 	$r[time] . ', ' .
-		// 		'"html":'		. 	$r["html"] . ', ' .
-		// 		'"css":' 		. 	$r["css"] . ', ' .
-		// 		'"special":'	.	$r["special"] . '} <br>';
+		
 		if ($r["url"] == "") continue;
 		$url = $r["url"];
 
 // Add JSON to row once all input has been combined for a single url
 		if(($i != 0 && $purl != $url) || $i > 200){
-			$combined = substr_replace($combined, "", -2);
-			$combined .= "]";
+			// $combined = substr_replace($combined, "", -2);
+			$json = json_encode($combined);
 			$pages[] = array(
 								"url" => $purl,
 								"time" => $ptime,
-								"session" => $combined
+								"session" => $json
 							);
-
-			$combined = "[";
+			unset($combined);
+			$i = 0;
 		}
 
-		$combined .= '{"clock":'.$r["time"].','.
-				'"html":"'.$r["html"].'",'.
-				'"css":"'.$r["css"].'",'.
-				'"special":"'.$r["special"].'"},';
+		$combined[] = array(
+			'clock' => $r["time"],
+			'html' => $r["html"],
+			'css' => $r["css"],
+			'special' => $r["special"]
+			);
 		$ptime = $r["time"];
 		$purl = $r["url"];
 		$i++;
 	}
 
-	$combined .= "]";
 
 //Export Sorted Data into a duplicate table to merge old data set and new data set
 	mysql_query("CREATE TABLE replay_combined LIKE replay_sessions");
 	mysql_query("INSERT INTO replay_combined SELECT * FROM replay_sessions");
 
-	echo "Number of rows in Old Data: " . count($pages);
+	echo "Number of rows in Old Data: " . count($pages) . "\n";
 
 	foreach ($pages as $key => $value) {
 		// if ($pages[$key]['url'] == "") $pages[$key]['url'] = "UNDEFINED";
-		echo $key . ": " . $value . "\n";
+		echo $key . ": " . var_dump($value) . "\n";
+		
 		$time = substr($value['time'], 0, 10);
 		$query = sprintf("INSERT INTO replay_combined (url, time, session) VALUES ('%s', '%s', '%s')", mysql_real_escape_string($value['url']), mysql_real_escape_string($time), mysql_real_escape_string($value['session']) );
 		// $query = "INSERT INTO replay_combined (url, time, session) VALUES ('{$value['url']}', '{$value['time']}', '{$value['session']}')";
