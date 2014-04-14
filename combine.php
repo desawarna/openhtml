368cca0089626c40062ad76406281b8228797f0f
Thomas Park <thomas@thomaspark.me>
2014-04-13 22:57:32 -0400

    script to merge two data schemas

--- combine.php
+++ combine.php
@@ -32,11 +32,15 @@
 		if(($i != 0 && $purl != $url) || $i > 200){
 			// $combined = substr_replace($combined, "", -2);
 			$json = json_encode($combined);
-			$pages[] = array(
-								"url" => $purl,
-								"time" => $ptime,
-								"session" => $json
-							);
+			if ($json != "") {
+
+				$pages[] = array(
+									"url" => $purl,
+									"time" => $ptime,
+									"session" => $json
+								);
+			}
+			
 			unset($combined);
 			$i = 0;
 		}
@@ -54,9 +58,10 @@
 
 
 //Export Sorted Data into a duplicate table to merge old data set and new data set
-	mysql_query("CREATE TABLE replay_combined LIKE replay_sessions");
-	mysql_query("INSERT INTO replay_combined SELECT * FROM replay_sessions");
+	mysql_query("CREATE TABLE replay_combined2 LIKE replay_sessions");
+	mysql_query("INSERT INTO replay_combined2 SELECT * FROM replay_sessions");
 
+	echo "<html><head><title>foo</title></head><body>";
 	echo "Number of rows in Old Data: " . count($pages) . "\n";
 
 	foreach ($pages as $key => $value) {
@@ -64,7 +69,7 @@
 		echo $key . ": " . var_dump($value) . "\n";
 		
 		$time = substr($value['time'], 0, 10);
-		$query = sprintf("INSERT INTO replay_combined (url, time, session) VALUES ('%s', '%s', '%s')", mysql_real_escape_string($value['url']), mysql_real_escape_string($time), mysql_real_escape_string($value['session']) );
+		$query = sprintf("INSERT INTO replay_combined2 (url, time, session) VALUES ('%s', '%s', '%s')", mysql_real_escape_string($value['url']), mysql_real_escape_string($time), mysql_real_escape_string($value['session']) );
 		// $query = "INSERT INTO replay_combined (url, time, session) VALUES ('{$value['url']}', '{$value['time']}', '{$value['session']}')";
 
 		 // VALUES ({$pages[$key]['url']}, {$pages[$key]['time']}, {$pages[$key]['session']})
